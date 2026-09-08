ae16a84ba3942f926cd0dcf8e61d17276b2227f8
SPxMM3R1 <roberto.ramos.dz@gmail.com>
2026-09-08 19:27:15 -0300

    Corrige solapamiento de la cuadricula en 0.3.7 (#9)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "cl.vibedlna.player"
         minSdk = 23
         targetSdk = 36
-        versionCode = 9
-        versionName = "0.3.6"
+        versionCode = 10
+        versionName = "0.3.7"
     }
 
     buildTypes {
